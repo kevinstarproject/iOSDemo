8cf41dca0f76ee98acf09c2a7af91937437690a3
emile <gwrx2005@gmail.com>
2020-06-11 17:13:19 +0800

    Initializing a new project

---
+++ GithubUserList/GithubUserList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubUserList
+//
+//  Created by Jung-Hua Liu on 2020/6/11.
+//  Copyright © 2020 Jung-Hua Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
